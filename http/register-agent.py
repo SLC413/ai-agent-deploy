6f581cffec848a1c78980bd0b74a369fc40476d8
Xiaohuolong <CherryMary@boxgmail.com>
2026-07-22 11:26:13 +0800

    feat: pass compute password from deployment to management platform

--- http/register-agent.py
+++ http/register-agent.py
@@ -90,6 +90,7 @@ r = subprocess.run([
         'skipConnectivityCheck': True,
         'computeEmail': os.environ.get('COMPUTE_EMAIL', ''),
         'computeUserId': os.environ.get('COMPUTE_USER_ID') or None,
+        'computePassword': os.environ.get('COMPUTE_PASSWORD') or None,
     }),
 ], capture_output=True, text=True, timeout=30)
 
